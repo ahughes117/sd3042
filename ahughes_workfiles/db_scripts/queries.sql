dab1e34f8b22974a4f69d258e4652f4a6e9c821d
Alex Hughes <alexhughes117@gmail.com>
2012-11-21 01:55:07 +0000

    4th query answered

--- ahughes_workfiles/db_scripts/queries.sql
+++ ahughes_workfiles/db_scripts/queries.sql
@@ -2,9 +2,22 @@
 
 /* Question 4
  * Can you list all the staff names and their manager's name, for a particular shop?
-  */
+ * I'll also need to write a procedure that takes the ShopID as a parameter.
+ */
   
-SELECT E.NAME, E.SURNAME, M.NAME, M.SURNAME
-FROM NH_EMPLOYEE E, NH_EMPLOYEE M
+SELECT E.Name, E.Surname, M.Name, M.Surname
+FROM NH_EMPLOYEE M, NH_EMPLOYEE E
 INNER JOIN NH_SHOP S ON S.ShopID = E.ShopID
-WHERE S.ManagerID = M.EmployeeID
+WHERE S.ManagerID = M.EmployeeID AND S.ShopID = ;
+
+/* Question 5
+ * Can you list all the sessions a specific member of staff had on a specific day, which
+ * customers and the room numbers?
+ */
+ 
+ 
+ 
+/* Question 6
+ * A customer could ask "Which member of staff did my 9-10 session today 
+ * and can you tell me who their manager is because I want to complain?
+ */
