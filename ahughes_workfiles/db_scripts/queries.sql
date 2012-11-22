a08d2181afbe69b79d2fd35fcac3eeb8220b89b3
Alex Hughes <alexhughes117@gmail.com>
2012-11-22 12:58:32 +0000

    More work on queries...

--- ahughes_workfiles/db_scripts/queries.sql
+++ ahughes_workfiles/db_scripts/queries.sql
@@ -2,32 +2,38 @@
 
 /* Question 4
  * Can you list all the staff names and their manager's name, for a particular shop?
- * I'll also need to write a procedure that takes the ShopID as a parameter.
  */
   
-SELECT E.Name AS Emp_Name, E.Surname AS Emp_Surname, M.Name AS Manager_Name, M.Surname AS Manager_Surname
+SELECT E.Name AS Emp_Name, E.Surname AS Emp_Surname, 
+M.Name AS Manager_Name, M.Surname AS Manager_Surname,
+S.ShopID, S.Address
 FROM NH_EMPLOYEE M, NH_EMPLOYEE E
 INNER JOIN NH_SHOP S ON S.ShopID = E.ShopID
-WHERE S.ManagerID = M.EmployeeID AND S.ShopID = ;
+WHERE S.ManagerID = M.EmployeeID AND S.ShopID = 2;
+
 
 /* Question 5
  * Can you list all the sessions a specific member of staff had on a specific day, which
  * customers and the room numbers?
  */
 
-SELECT B.BookingID, B."Date", C.Name, C.Surname, R.RoomID, R.Name
+SELECT B.BookingID, B."Date", C.Name, C.Surname, R.RoomID, 
+R.Name AS Room_Name
 FROM NH_BOOKING B
 INNER JOIN NH_EMPLOYEE E ON E.EmployeeID = B.EmployeeID
 INNER JOIN NH_ROOM R ON R.RoomID = B.RoomID
 INNER JOIN NH_CUSTOMER C ON C.CustomerID = B.CustomerID
-WHERE E.EmployeeID = 2 AND B."Date" = '05-JUN-12';
+WHERE E.EmployeeID = 5 AND B."Date" = '11-JUL-11';
+
  
 /* Question 6
  * A customer could ask "Which member of staff did my 9-10 session today 
  * and can you tell me who their manager is because I want to complain?
  */
  
-SELECT B.BookingID, B."Date", E.EmployeeID, E.NAME, E.Surname, M.Name, M.Surname
+SELECT B.BookingID, B."Date", 
+  E.NAME AS Emp_Name, E.Surname AS Emp_Surname, 
+  M.Name AS Mgr_Name, M.Surname AS Mgr_Surname
 FROM NH_EMPLOYEE M, NH_BOOKING B
 INNER JOIN NH_CUSTOMER C ON C.CustomerID = B.CustomerID
 INNER JOIN NH_EMPLOYEE E ON E.EmployeeID = B.EmployeeID
