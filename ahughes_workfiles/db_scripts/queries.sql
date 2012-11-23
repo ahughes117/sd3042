ca772aca42b44bed3072252913efb82aabafd4b1
Alex Hughes <alexhughes117@gmail.com>
2012-11-23 19:23:12 +0000

    Finished with the queries

--- ahughes_workfiles/db_scripts/queries.sql
+++ ahughes_workfiles/db_scripts/queries.sql
@@ -31,7 +31,7 @@ WHERE E.EmployeeID = 5 AND B."Date" = '11-JUL-11';
  * and can you tell me who their manager is because I want to complain?
  */
  
-SELECT B.BookingID, B."Date", 
+SELECT B.BookingID, TO_CHAR(B."Date",'hh24:mi:ss') AS DATETIME,
   E.NAME AS Emp_Name, E.Surname AS Emp_Surname, 
   M.Name AS Mgr_Name, M.Surname AS Mgr_Surname
 FROM NH_EMPLOYEE M, NH_BOOKING B
@@ -39,7 +39,8 @@ INNER JOIN NH_CUSTOMER C ON C.CustomerID = B.CustomerID
 INNER JOIN NH_EMPLOYEE E ON E.EmployeeID = B.EmployeeID
 INNER JOIN NH_ROOM R ON R.RoomID = B.RoomID
 INNER JOIN NH_SHOP S ON S.ShopID = E.ShopID
-WHERE S.ManagerID = M.EmployeeID;
+WHERE S.ManagerID = M.EmployeeID 
+AND B."Date" = to_date('04/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam');
 
 
 /* In order to make the queries a little bit more dynamic to parameters, we will use stored
