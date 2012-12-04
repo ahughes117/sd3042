4ab07eb7e99dd3c99e734689546d6f37fd233695
Alex Hughes <alexhughes117@gmail.com>
2012-12-04 02:23:15 +0000

    Modified changelog for final document, probably final version, housekeeping and minor changes on db_scripts

--- ahughes_workfiles/db_scripts/queries/check_bookings.sql
+++
@@ -1,2 +0,0 @@
-SELECT BookingID, RoomID, EmployeeID, CustomerID, TreatmentID, TO_CHAR("Date",'dd/mm/yy hh24:mi:ss') 
-FROM NH_BOOKING;
---
+++ ahughes_workfiles/db_scripts/queries/good_booking.sql
@@ -0,0 +1,14 @@
+/* We will now try to see whether our discount trigger works */
+
+exec new_booking(11, 12, 14, 11, to_date('03/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('06/07/2010:03:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('08/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('10/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('12/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('14/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('16/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('18/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('20/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('22/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+exec new_booking(11, 12, 14, 11, to_date('24/07/2010:01:00:00PM', 'dd/mm/yyyy:hh:mi:ssam'), 100);
+
